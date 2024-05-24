5cc39a3b51b730ca0d45ba4505028c51778b5700
Ian Firdaus <ianfirdaus31@gmail.com>
2024-05-24 14:29:34 +0700

    Add hello world get url with string return

---
+++ src/main/java/com/ianfirdaus/springboot1/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.ianfirdaus.springboot1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+}
